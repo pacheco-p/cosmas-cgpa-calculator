5348d8ca1651cc903556eb0b0f1acbf7d1beba67
pacheco-p <pedroprince53@gmail.com>
2026-06-28 01:30:05 +0100

    app.py

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import streamlit as st
 import database
+import auth
+
+database.init_db()
 
 st.set_page_config(
     page_title="Cosmas CGPA Calculator",
@@ -7,9 +10,79 @@ st.set_page_config(
     layout="wide"
 )
 
-database.init_db()
+if "logged_in" not in st.session_state:
+    st.session_state.logged_in = False
+
+if "user" not in st.session_state:
+    st.session_state.user = None
 
 st.title("🏛️ COSMAS AT SUG TOP SEAT")
 st.caption("Support • Pray • Canvass")
 
-st.success("Database initialized successfully ✅")
+if not st.session_state.logged_in:
+
+    tab1, tab2 = st.tabs(["Login", "Create Account"])
+
+    with tab1:
+
+        username = st.text_input("Username")
+
+        password = st.text_input(
+            "Password",
+            type="password"
+        )
+
+        if st.button("Login"):
+
+            user = auth.login(username, password)
+
+            if user:
+
+                st.session_state.logged_in = True
+                st.session_state.user = user
+
+                st.success("Login Successful")
+
+                st.rerun()
+
+            else:
+
+                st.error("Invalid Username or Password")
+
+    with tab2:
+
+        username = st.text_input("Username", key="u")
+
+        email = st.text_input("Email")
+
+        password = st.text_input(
+            "Password",
+            type="password",
+            key="p"
+        )
+
+        if st.button("Create Account"):
+
+            if auth.register(
+                username,
+                email,
+                password
+            ):
+
+                st.success("Account Created Successfully")
+
+            else:
+
+                st.error("Username or Email already exists.")
+
+else:
+
+    st.success(
+        f"Welcome {st.session_state.user[1]} 👋"
+    )
+
+    if st.button("Logout"):
+
+        auth.logout()
+
+        st.rerun()
