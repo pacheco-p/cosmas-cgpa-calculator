9d1303add17951899b7e98009e0e3c7066dc5d01
pacheco-p <pedroprince53@gmail.com>
2026-06-28 02:06:02 +0100

    dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,23 +1,53 @@
 import streamlit as st
+from database import cursor
 
 def dashboard():
 
-    st.title("🏛 COSMAS Dashboard")
+    st.title("🏛️ Dashboard")
 
     st.success(
-        f"Welcome {st.session_state.username} 👋"
+        f"Welcome back, {st.session_state.username} 👋"
     )
 
-    st.metric(
-        "Saved Calculations",
-        "0"
+    cursor.execute(
+        """
+        SELECT COUNT(*)
+        FROM history
+        WHERE username=?
+        """,
+        (st.session_state.username,)
     )
 
-    st.metric(
-        "Current CGPA",
-        "0.00"
+    total = cursor.fetchone()[0]
+
+    col1, col2 = st.columns(2)
+
+    col1.metric(
+        "Saved Results",
+        total
     )
+
+    cursor.execute(
+        """
+        SELECT MAX(cgpa)
+        FROM history
+        WHERE username=?
+        """,
+        (st.session_state.username,)
+    )
+
+    highest = cursor.fetchone()[0]
+
+    if highest is None:
+        highest = 0
+
+    col2.metric(
+        "Highest CGPA",
+        f"{highest:.2f}"
+    )
+
+    st.divider()
 
     st.info(
-        "Use the sidebar to navigate."
+        "Use the sidebar to access your calculator, history and profile."
     )
